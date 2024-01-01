c97f926b6e585f7be45265b6941acc8f66cfab43
Yutao Zhou <91305696+Yutao-Zhou@users.noreply.github.com>
2024-01-01 15:22:30 -0800

    Deploy to GitHub Pages

---
+++ precache-manifest.b252efe45e88a691f62358c7ac7fe105.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04169b3a4a21803549f93b26227052ac",
+    "url": "/showroom-talks/index.html"
+  },
+  {
+    "revision": "5fd25001a2663af580e0",
+    "url": "/showroom-talks/static/css/2.3f4f504a.chunk.css"
+  },
+  {
+    "revision": "e42f08202da6428be3a8",
+    "url": "/showroom-talks/static/css/main.dd318d6a.chunk.css"
+  },
+  {
+    "revision": "5fd25001a2663af580e0",
+    "url": "/showroom-talks/static/js/2.b097ac33.chunk.js"
+  },
+  {
+    "revision": "c7df833156a1790b4f9c0891a7e831c3",
+    "url": "/showroom-talks/static/js/2.b097ac33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ce6fdc2781f40147da",
+    "url": "/showroom-talks/static/js/3.89d07189.chunk.js"
+  },
+  {
+    "revision": "e42f08202da6428be3a8",
+    "url": "/showroom-talks/static/js/main.606ca9ff.chunk.js"
+  },
+  {
+    "revision": "b327681c5544f947e1db",
+    "url": "/showroom-talks/static/js/runtime-main.99ac025c.js"
+  },
+  {
+    "revision": "1931427c23716bb4ff98d03627f7137c",
+    "url": "/showroom-talks/static/media/fa-brands-400.1931427c.ttf"
+  },
+  {
+    "revision": "b6356c957274676e6571c1ff5e11c9a8",
+    "url": "/showroom-talks/static/media/fa-brands-400.b6356c95.woff2"
+  },
+  {
+    "revision": "023a4a925fa3fce0f66b769ef6bbb264",
+    "url": "/showroom-talks/static/media/fa-regular-400.023a4a92.woff2"
+  },
+  {
+    "revision": "a0cc1c8265e3163aa654a5284ea11ace",
+    "url": "/showroom-talks/static/media/fa-regular-400.a0cc1c82.ttf"
+  },
+  {
+    "revision": "25914cfeafced317e1a0372187fbb2b9",
+    "url": "/showroom-talks/static/media/fa-solid-900.25914cfe.ttf"
+  },
+  {
+    "revision": "6c4eee562650e53cee32496bdfbe534b",
+    "url": "/showroom-talks/static/media/fa-solid-900.6c4eee56.woff2"
+  },
+  {
+    "revision": "71808e147dc6d82c198a4ba292c0cb69",
+    "url": "/showroom-talks/static/media/fa-v4compatibility.71808e14.ttf"
+  },
+  {
+    "revision": "c9e50ccabe9e36f370272197595ea1e5",
+    "url": "/showroom-talks/static/media/fa-v4compatibility.c9e50cca.woff2"
+  }
+]);
