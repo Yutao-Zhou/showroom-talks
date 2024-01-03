7549962042feb1e618c700f507fbf54de269ab3e
Yutao Zhou <91305696+Yutao-Zhou@users.noreply.github.com>
2024-01-03 15:47:01 -0800

    Add post for BMW 550i(F10)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import './Home.css';
 function Home() {
     const posts = [
         { id: 1, title: 'Tesla Model 3', date: 'January 2, 2024', content: 'The Tesla Model 3 offered impressive acceleration and range but had issues with heavy steering, higher noise levels, and handling, making it a compelling choice for new electric car owners despite challenges for traditional car drivers.' },
-        { id: 2, title: 'BMW 550i', date: 'January 1, 2024', content: '' },
+        { id: 2, title: 'BMW 550i(F10)', date: 'January 3, 2024', content: 'BMW\'s F10 550i sedan offers a blend of practicality and exhilarating performance, showcasing a robust chassis, agile powertrain, and serene cabin, making it a top choice in its class despite concerns about the N63TU engine\'s reliability.' },
         { id: 3, title: 'Jeep Wrangler', date: 'January 1, 2024', content: '' },
         { id: 4, title: 'KIA Soul', date: 'January 1, 2024', content: '' },
         { id: 5, title: 'Toyota Camry', date: 'January 1, 2024', content: '' },
